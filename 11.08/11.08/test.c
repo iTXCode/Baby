b49eb20bbfc2069371e9b739b274dc9f657d33a9
jack-wang128801 <1632672310@qq.com>
2018-11-08 23:02:47 +0800

    两个例题

---
+++ 11.08/11.08/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//  //两个数比特位不同相加
+//int count_every_bit(unsigned int x,unsigned int  y)
+//{
+//	int count=0;
+//	int  i=0;
+//	for(i=0;i<32;i++)
+//	{
+//		if(((x>>i) & (y>>i))==0)
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+//
+//
+//int main()
+//{
+//	int m=0;
+//    int n=0;
+//	int ret=0;
+//	scanf("%u\n",&m);
+//    scanf("%u",&n);
+//     ret=count_every_bit(m,n);
+//	 printf("%d\n",ret);
+//	return 0;
+//}
+
+
