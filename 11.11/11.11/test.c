20d98948eae1760c65eabd333df1ca25ea902131
jack-wang128801 <1632672310@qq.com>
2018-11-11 22:19:28 +0800

    写博客用到的代码

---
+++ 11.11/11.11/test.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(char));
+//	printf("%d\n",sizeof(short));
+//	printf("%d\n",sizeof(int ));
+//	printf("%d\n",sizeof(long));
+//	printf("%d\n",sizeof(long long));
+//	printf("%d\n",sizeof(float));
+//	printf("%d\n",sizeof(double));
+//
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(void));
+//	printf("%d\n",sizeof(void*));
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//    char a=-1;
+//	signed char b=-1;
+//	unsigned char c=-1;
+//	printf("a=%d,b=%d,c=%d\n",a,b,c);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a=-20;
+//	signed int b=-20;
+//	unsigned int c=-20;
+//	printf("a=%d,b=%d,c=%u\n",a,b,c);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a=20;
+//	printf("a=%d\n",a);
+//	return 0;
+//}
