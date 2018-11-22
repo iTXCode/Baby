5c23c49abcb0a16ab7655c4abce82502795791de
jack-wang128801 <1632672310@qq.com>
2018-11-22 23:25:14 +0800

    2018.11.22 星期四 晴 今天刷了一天的网课，没有好好敲代码。感觉心里空落落的。因为今天上课提到了冒泡排序 法，所以晚上回来实现了一下冒泡排序法。这个算法看似简单，但是其蕴含的思想值得深究 。利用两次循环实现了排序的目的。

---
+++ 11.22/11.22/test.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+//	char arr1[32]="123abc456efd789ghi";
+//	//char arr2[32];
+//    printf("%s\n",memmove(arr1,arr1+1,strlen(arr1)));
+//	return 0;
+//}
+
+
+//#include<stdio.h>       //∆’Õ®µƒ√∞≈›≈≈–Ú∑®
+//void print(int *arr, int n)
+//{
+//	int i;
+//	for(i=0;i<n;i++)
+//	{
+//       printf("%d ",*arr);
+//	   arr++;
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[]={1,2,6,3,9,8,7,5,4,0};
+//	int n=sizeof(arr)/sizeof(arr[0]);
+//	int max=0;
+//	int i,j;
+//	for(i=0;i<n-1;i++)
+//	{
+//		for(j=0;j<9-i;j++)
+//		{
+//			if(arr[j]>arr[j+1])
+//			{
+//				max=arr[j];
+//				arr[j]=arr[j+1];
+//				arr[j+1]=max;
+//			}
+//		
+//		}
+//	}
+//	print(arr,n);
+//	printf("\n");
+//	return 0;
+//}
+
+
