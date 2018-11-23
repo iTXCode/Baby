8fd3022ccd6aa600c84f0409c1ed350967439da3
jack-wang128801 <1632672310@qq.com>
2018-11-23 23:56:43 +0800

    2018.11.23 星期五  晴 今天比较忙吧，去参加了蓝桥杯的选拔。感觉自己好菜。 我开始产生怀疑这半个月来的学习难道就这么没用吗？ 为什么连那几个体都做不出来。 好失落啊。 但是生活还要继续，只能在失败中寻找不足，积攒经验。不断反省自己才行。

---
+++ 11.23/11.23/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//                    //内存对齐的一些练习
+//struct S1
+//{
+//	char i;
+//	int a;
+//	char b;
+//};
+//
+//
+//struct S2
+//{
+// char c1;
+// char c2;
+// int i;
+//};
+//
+//struct S3
+//{
+// double d;
+// char c;
+// int i;
+//};
+//
+//struct S4
+//{
+// char c1;
+// struct S3 s3;
+// double d;
+//};
+//
+//int main()
+//{
+//	printf("%d\n",sizeof(struct S1));
+//	printf("%d\n",sizeof(struct S2));
+//	printf("%d\n",sizeof(struct S3));
+//	printf("%d\n",sizeof(struct S4));
+//	return 0;
+//}
