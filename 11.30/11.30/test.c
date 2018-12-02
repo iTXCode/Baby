053247326b89082d1a442bfb63371f82e22b4a1c
jack-wang128801 <1632672310@qq.com>
2018-12-02 22:48:37 +0800

    2018.11.30 之前的出错了，重新传一下。

--- 11.30/11.30/test.c
+++ 11.30/11.30/test.c
@@ -21,53 +21,53 @@
 //}
 
 
-//
-//#include<stdio.h>     //模拟实现strncat
-//#include<string.h>
-//#include<assert.h>
-//
-//void my_strncat(char *arr, const char *arr1,int n)
-//{
-//	char *p=arr;
-//	char *q=(char *)arr1;
-//	assert(arr);
-//	assert(arr1);
-//	while(*p )
-//	{
-//        p++;
-//	}
-//	while(n--)
-//	{
-//      *p=*q;
-//	  p++;
-//	  q++;
-//	}
-//	*p='\0';
-//	printf("%s",arr);
-//	printf("\n");
-//}
-//
-//int main()
-//{
-//	char arr[32]="I am a ";
-//	const char arr1[]="good student!";
-//	int num;
-//	printf("请输入要拼接字符长度(num<%d):",strlen(arr1)+1);
-//	scanf("%d",&num);
-//	my_strncat(arr,arr1,num);
-//	return 0;
-//}
-//
-//
 
-
-#include<stdio.h>
+#include<stdio.h>     //模拟实现strncat
 #include<string.h>
+#include<assert.h>
+
+void my_strncat(char *arr, const char *arr1,int n)
+{
+	char *p=arr;
+	char *q=(char *)arr1;
+	assert(arr);
+	assert(arr1);
+	while(*p )
+	{
+        p++;
+	}
+	while(n--)
+	{
+      *p=*q;
+	  p++;
+	  q++;
+	}
+	*p='\0';
+	printf("%s",arr);
+	printf("\n");
+}
 
 int main()
 {
-	char arr[32]="I am a";
-	char arr1[]=" good student!";
-	printf("%s\n",strncat(arr,arr1,strlen(arr1)+1));
+	char arr[32]="I am a ";
+	const char arr1[]="good student!";
+	int num;
+	printf("请输入要拼接字符长度(num<%d):",strlen(arr1)+1);
+	scanf("%d",&num);
+	my_strncat(arr,arr1,num);
 	return 0;
 }
+
+
+
+
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+//	char arr[32]="I am a";
+//	char arr1[]=" good student!";
+//	printf("%s\n",strncat(arr,arr1,strlen(arr1)+1));
+//	return 0;
+//}
